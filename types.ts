b12233f9f1b7cbe004035da40b2eeafbdb5a80ea
SilentBYOB <7892580@gmail.com>
2025-10-17 13:19:07 +0200

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,32 @@
+export interface Diagnosis {
+    name: string;
+    probability: number;
+    short_explanation: string;
+    long_explanation: string;
+    recommendations: string[];
+    icon: string;
+}
+
+export interface Consultation {
+    id: string;
+    date: string;
+    symptoms: string;
+    questions: string[];
+    answers: string[];
+    diagnoses: Diagnosis[];
+    selectedDiagnosis: Diagnosis;
+}
+
+export enum SubscriptionLevel {
+    Free = 'FREE',
+    Pro = 'PRO',
+}
+
+export enum AppState {
+    Welcome,
+    SymptomInput,
+    Question,
+    Diagnosis,
+    Treatment,
+    History,
+}
